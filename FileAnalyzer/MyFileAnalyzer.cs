5bd41dd98521f65e23982b26c0edcda32edd42a5
Alex Winter <bannedsaveplay@yandex.ru>
2025-06-20 03:27:57 +0300

    Добавьте файлы проекта.

---
+++ FileAnalyzer/MyFileAnalyzer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAnalyzer
+{
+    public class MyFileAnalyzer
+    {
+        private string _path;
+
+        private string _content;
+
+        public record WordFrequency(string Word, int Count);
+        public MyFileAnalyzer(string path) 
+        {
+            _path = path;
+
+            if (!System.IO.File.Exists(_path))
+            {
+                throw new System.IO.FileNotFoundException("File not found", _path);
+            }
+            _content = System.IO.File.ReadAllText(_path);
+        }
+
+        public string GetFileName()
+        {
+            return System.IO.Path.GetFileName(_path);
+        }
+
+        public int GetNumberWords()
+        {
+            var words = _content.Split(new[] {' ', '\n', '\r', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Length;
+        }
+
+        public int GetNumberUniqueWords()
+        {
+            var words = _content.Split(new[] { ' ', '\n', '\r', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Select(word => word.ToLowerInvariant()).Distinct().Count();
+        }
+
+        public List<WordFrequency> GetTop5Words()
+        {
+            var words = _content.Split(new[] { ' ', '\n', '\r', ',', '.' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(word => word.ToLowerInvariant())
+                .GroupBy(word => word)
+                .Select(group => new WordFrequency(group.Key, group.Count()))
+                .OrderByDescending(wf => wf.Count)
+                .Take(5)
+                .ToList();
+
+            return words;
+        }
+
+        public int GetNumberLargestWord()
+        {
+            var words = _content.Split(new[] { ' ', '\n', '\r', ',', '.' }, StringSplitOptions.RemoveEmptyEntries);
+            return words.Select(word => word.Length).DefaultIfEmpty(0).Max();
+        }
+
+        public int GetNumberLines()
+        {
+            return _content.Split(['\n'], StringSplitOptions.RemoveEmptyEntries).Length;
+        }   
+    }
+}
---
+++ FileAnalyzer/Program.cs
@@ -0,0 +1,23 @@
+﻿using FileAnalyzer;
+
+string myFilePath = @"C:\Users\PC\source\repos\FileAnalyzer\FileAnalyzer\TextFile.txt";
+
+MyFileAnalyzer analyzer = new MyFileAnalyzer(myFilePath);
+
+Console.WriteLine($"Имя файла: {analyzer.GetFileName()}");
+
+Console.WriteLine($"Количество слов: {analyzer.GetNumberWords()}");
+
+Console.WriteLine($"Количество уникальных слов: {analyzer.GetNumberUniqueWords()}");
+
+Console.WriteLine("Топ 5 слов:");
+foreach (var wordFreq in analyzer.GetTop5Words())
+{
+    Console.WriteLine($"{wordFreq.Word}: {wordFreq.Count}");
+}
+
+Console.WriteLine($"Длина самого длинного слова: {analyzer.GetNumberLargestWord()}");
+
+Console.WriteLine($"Количество строк в файле: {analyzer.GetNumberLines()}");
+
+Console.ReadLine();
